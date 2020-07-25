5e318f72ea45e69a417c91514acd50120b805909
Julio Gomez <jgod93@Julios-MacBook-Pro-20.local>
2020-07-24 20:33:26 -0400

    MvP

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { useReducer, useState } from "react";
+import {initialState, reducer }from "./reducers/reducer";
+import TodoList from './components/TodoList'
+// import './App.css'
+import '../src/components/Todo.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const [newTodo, setNewTodo] = useState("");
+    //this hook controls  the input in the form
+    const [state, dispatch] = useReducer(reducer, initialState);
+    //this hook controls all state
+
+      const handleChanges = e => {
+        setNewTodo(e.target.value)
+      }
+    //an event listener that changes the default value of newTodo to whatever the user
+    //types in the input
+    
+      const toggleItem = itemId => {
+        dispatch({ type: "CHANGE_KEY", payload: itemId })
+      };
+    
+      const clearCompleted = () => {
+       dispatch({ type: "CLEAR_COMPLETED" })
+      }
+    
+    return (
+        <div>
+           <h2>Welcome to your Todo-Reducer App!</h2>
+            <div className="form">
+                <input 
+                className='input'
+                type='text'
+                value = { newTodo }
+                name = 'item'
+                onChange = { handleChanges }
+                placeHolder = 'Add a Task...'
+                />
+            <button onClick={() => {
+               dispatch({ type: "SET_TODO", payload: newTodo });
+              }}> + </button>
+            </div>
+        <TodoList 
+          list = { state } 
+          toggleItem = { toggleItem }
+        /> 
+        <button className="clear-button"
+            onClick={ clearCompleted }
+            > Clear Completed 
+        </button>
+        </div>
+    );
 }
 
 export default App;
+
--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,18 +1,55 @@
-import React, { useReducer } from 'react';
+export const initialState = [
+{
+    task: 'Create To do App',
+    id: 1528817084359,
+    completed: false
+},
+{
+    task: 'Finish Project',
+    id: 1528817077286,
+    completed: false
+},
+{
+    task: 'Study',
+    id: 1528817084358,
+    completed: false
+},
+{
+    task: 'Walk Dog',
+    id: 1528817084355,
+    completed: false
+}
+]
 
-export const initialState = {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
- }
-const reducer = (state, action) => {
+export const reducer = (state, action) => {
     switch(action.type) {
-        
+        case  "SET_TODO":
+            const newItem = {
+                task: action.payload,
+                id: Date.now(),
+                completed: false
+            }
+      return [
+        ...state,
+        newItem
+      ]
+      case "CHANGE_KEY":
+
+        return [
+            ...state.map(todo => action.payload === todo.id ? {...todo, completed: !todo.completed} : todo)
+        ]
+        case "CLEAR_COMPLETED":
+            
+        return [
+            ...state.filter(item => !item.completed)
+        ]
+
+    default:
+      return state;
+  }
+ };
 
 
-        default:
-            return state;
-    };
-};
 
-export default reducer;
+
+
